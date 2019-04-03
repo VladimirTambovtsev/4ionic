1beca3780d1bb169bd4db72370ab7f5db75eced8
Vladimir Tambovtsev <tambovcev99@mail.ru>
2019-04-03 23:52:57 +0300

    add virtual-scroll

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -9,10 +9,12 @@ import { Place } from '../place.model';
 })
 export class DiscoverPage implements OnInit {
   loadedPlaces: Place[];
+  listedLoadedPlaces: Place[];
 
   constructor(private placesService: PlacesService) {}
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.getPlaces;
+    this.listedLoadedPlaces = this.loadedPlaces.slice(1);
   }
 }
