26767588625962d054b80903c471a38a7fc94f8e
Vladimir Tambovtsev <tambovcev99@mail.ru>
2019-04-03 22:57:46 +0300

    add swiping items

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,6 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+import { IonItemSliding } from '@ionic/angular';
 import { PlacesService } from './../places.service';
 import { Place } from './../place.model';
-import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-offers',
@@ -10,9 +12,15 @@ import { Component, OnInit } from '@angular/core';
 export class OffersPage implements OnInit {
   offers: Place[];
 
-  constructor(private placesService: PlacesService) {}
+  constructor(private placesService: PlacesService, private router: Router) {}
 
   ngOnInit() {
     this.offers = this.placesService.getPlaces;
+  }
+
+  onEdit(offerId: string, slidingItem: IonItemSliding) {
+    slidingItem.close();
+    this.router.navigate(['/', 'places', 'tabs', 'offers', 'edit', offerId]);
+    console.log(offerId);
   }
 }
